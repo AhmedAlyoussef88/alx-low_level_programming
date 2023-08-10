1223d4677ac8ca2881c6fcb30b5eaf178f370bb1
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-10 15:12:08 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,15 @@
 
 /**
  * main - Entry point
- *
+ * Description: Display datatype with size
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    return (0);
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+   	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
