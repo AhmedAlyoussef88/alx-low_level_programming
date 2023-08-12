231d1617f902021ed46b6445dd00792de5a6a1db
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-11 18:26:20 -0700

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Print all possible combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + 48);
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+
+	putchar('\n');
+	return (0);
+}
