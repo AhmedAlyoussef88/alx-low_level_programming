a555847a05d080f7f6423845663db56813e1949a
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-11 17:42:53 -0700

    my commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Display all letters except q and e
+* Return: Always 0 (Success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' && l != 'e')
+		{
+			putchar(l);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
