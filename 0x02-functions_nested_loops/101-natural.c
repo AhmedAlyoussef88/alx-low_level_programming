6615524e7fa8310f558cf960a38b1476dfd4895d
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-15 16:09:21 -0700

    my commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: list all the natural numbers below 10
+ * Return: Always 0 (Success)
+ */
+void main(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			x += y;
+		}
+		x++;
+	}
+	printf("d\n", x);
+	return (0);
+}
