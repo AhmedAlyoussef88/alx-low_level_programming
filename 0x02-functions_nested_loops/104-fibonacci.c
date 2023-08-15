48f9e6e363fbf848579b127d84a9f54127ba03bb
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-15 16:38:46 -0700

    my commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:  finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
+
