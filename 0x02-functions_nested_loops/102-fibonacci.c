776683940132b2b610912840cb32f0fb929a9215
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-15 16:26:33 -0700

    my commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints the first 50 Fibonacci numbers
+ * Return: Always 0 (Success)
+ */
+void main(void)
+{
+	int count = 0;
+	unsigned long x = 0, y = 1, z;
+
+
+	while (count < 50)
+	{
+		z = x + y;
+		printf("%lu", z);
+
+		x = y;
+		y = z;
+
+		if (count == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		count++;
+	}
+}
