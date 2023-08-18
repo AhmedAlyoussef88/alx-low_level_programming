fc707339a8d43119386c11cbf74d5bab643bff6e
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-17 19:51:48 -0700

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * most_numbers - prints 10 times the numbers,
+ * more_numbers - prints 10 times the numbers,
  * from 0 to 14, followed by a new line
  */
 
-void most_numbers(void)
+void more_numbers(void)
 {
 	int x, y;
 
