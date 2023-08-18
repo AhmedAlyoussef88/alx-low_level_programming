3110f4ca1036bc6d9e486e36f5eae0e7156d6683
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-17 17:55:17 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 10 times the numbers,
+ * from 0 to 14, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int i = 0, j;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
