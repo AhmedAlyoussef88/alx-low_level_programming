39a40f6b267513cf33a05d3eb7f8828b419670c9
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-21 16:15:53 -0700

    my commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,20 +6,23 @@
  */
 int _atoi(char *s)
 {
-	int sign = 0;
+	int sign = 0, found = 0, retresult = 0;
 
 	unsigned int number = 0;
 
-	int retResult = 0;
-
-	while (*s++)
+	while (*s)
 	{
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
+		{
 			number = (number * 10) + (*s - '0');
-		else if (number > 0)
+			found = 1;
+		}
+		else if (found)
 			break;
+
+		s++;
 	}
 	retResult = number * sign;
 	return (retResult);
