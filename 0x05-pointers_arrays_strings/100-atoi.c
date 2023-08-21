9f78329304bcaf3e96889eb399e80124c6e9c6a3
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-21 15:55:23 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: Pointer parameter string
+ *Return: converted srting
+ */
+int _atoi(char *s)
+{
+	int sign = 0;
+
+	unsigned int number = 0;
+
+	int retResult = 0;
+
+	while (1)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			number = (number * 10) + (*s - '0');
+		else if (number > 0)
+			break;
+		*s++;
+	}
+	retResult = number * sign;
+	return (retResult);
+}
