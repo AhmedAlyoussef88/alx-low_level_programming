6692501f65ee2a670bc25f54ec7349b14f5ca42f
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-21 13:15:24 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: Pointer Parameter
+ * @n: int parameter
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
