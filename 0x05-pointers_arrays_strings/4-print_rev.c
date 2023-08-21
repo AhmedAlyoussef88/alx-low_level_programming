e295eb94e6fde7e7f9c510cd5779e8aa31f32ba7
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-21 12:32:35 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: Pointer Parameter
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	int j;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	_putchar('\n');
+}
