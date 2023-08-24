94ff23adb1e83aba4eabbc246464979154fd75ba
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-24 15:35:29 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *leet - function that encodes a string into 1337
+ * @str: string input parameter
+ * Return: result string
+ */
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+
+	char key[] = "aAeEoOtTlL";
+	char value[] = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		while (j < 10)
+		{
+			if (key[i] == value[j])
+			{
+				srt[i] = value[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
