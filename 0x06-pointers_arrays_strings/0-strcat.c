12abcaee164ee0a374e674177882fcaea6833f01
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-24 11:38:31 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: Concatenation result
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count1 = 0, count2 = 0;
+
+	while (src[count1] != '\0')
+	{
+		count1++;
+	}
+
+	while (src[count2] != '\0')
+	{
+		dest[count1] = src[count2];
+		count1++;
+		count2++
+	}
+	dest[count2] = '\0';
+
+	return (dest);
+}
