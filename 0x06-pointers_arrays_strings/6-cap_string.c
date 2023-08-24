db15714b608315c65f9cc772d24de1de002a2f2e
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-24 14:28:01 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * *cap_string - Separators of words: space, tabulation
+ * @str: string input parameter
+ * Return: result string
+ */
+char *cap_string(char *str)
+{
+	char speparators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
+
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		while (separators[j] != '\0')
+		{
+			if (str[i] == separators[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] = str[i + 1] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (str);
+}
