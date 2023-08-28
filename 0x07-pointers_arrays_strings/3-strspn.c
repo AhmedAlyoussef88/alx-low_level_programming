515869f46d0e902d15b91ed309739194137ce509
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-28 09:04:27 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: Input parameter string
+ * @accept: Input parameter string
+ *
+ * Return: number of bytes in the initial segment of s
+ which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j, x = 0, y;
+
+	while (s[i] != '\0')
+	{
+		y = 0;
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				x++;
+				y = 1;
+			}
+			j++;
+		}
+		if (y == 0)
+			return (x);
+
+		i++;
+	}
+	return (0);
+}
