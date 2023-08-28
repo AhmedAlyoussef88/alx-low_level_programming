c169cfba0817bedbc0c4e36eb130982bea76516a
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-28 09:29:09 -0700

    my commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,23 +4,24 @@
  * @s: Input parameter string
  * @accept: Input parameter string
  * Return: number of bytes in the initial segment of s
- which consist only of bytes from accept
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0;
+	int i = 0, j = 0;
 
-	while (s[i] != '\0')
+	unsigned int x = 0;
+
+	while (s[i] != '\0' && s[i] != ' ')
 	{
-		j = 0;
 		while (accept[j] != s[i])
 		{
 			if (accept[j] == '\0')
-				return (i);
+				x++;
 			j++;
 		}
-
+		j = 0;
 		i++;
 	}
-	return (i);
+	return (x);
 }
