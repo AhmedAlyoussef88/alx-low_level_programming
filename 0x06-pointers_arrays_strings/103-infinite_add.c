8170e2ff78053b386b2719f5dd5344764bb1f3d0
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-08-28 10:22:16 -0700

    my commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,50 +13,5 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	while (n1[i] != '\0')
-		i++;
-	
-	int j = 0;
-	
-	while (n2[j] != '\0')
-		j++;
-	
-	if (i > size_r - 1 || j > size_r - 1)
-		return NULL;
-
-    int m = 0;
-    int k = 0;
-
-    for (i -= 1, j -= 1; k < size_r - 1; i--, j--, k++)
-    {
-        int n = m;
-        if (i >= 0)
-            n += n1[i] - '0';
-        if (j >= 0)
-            n += n2[j] - '0';
-
-        if (i < 0 && j < 0 && n == 0)
-            break;
-
-        m = n / 10;
-        r[k] = n % 10 + '0';
-    }
-    r[k] = '\0';
-
-    if (i >= 0 || j >= 0 || m)
-        return NULL;
-
-    int l = 0;
-    while (l < k)
-    {
-        m = r[k];
-        r[k] = r[l];
-        r[l] = m;
-
-        k--;
-        l++;
-    }
-
-    return r;
+	return r;
 }
