2b86121465cc0c78c118216ec621478271275aa5
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-01 14:17:49 -0700

    my commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: Input parameter int
+ * @argv: Input parameter array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
+
