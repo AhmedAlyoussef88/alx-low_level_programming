52c5ae5d3b16dbf9414ff1fcd65aa62bfc04dd38
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-01 14:33:18 -0700

    my commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: input parameter int
+ * @argv: input parameter array
+ * Return: Success (0), fail (1)
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1] * atoi(argv[2]));
+		printf("%d\n", x);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+
+}
