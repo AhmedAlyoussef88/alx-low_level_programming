8da28ddffbde5ca21ed471fc5556a59386eb9c39
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-01 14:53:31 -0700

    my commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: input parameter int
+ * @argv: input parameter array
+ * Return: success (0), fail (1)
+ */
+int main(int argc, char *argv[])
+{
+	int y = 0, z = 0;
+
+	int i = 1, j = 0, x = 0;
+
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+		i++;
+	}
+	x = 1;
+	while (x < argc)
+	{
+		z = atoi(argv[x]);
+		if (z >= 0)
+		{
+			y += z;
+		}
+		x++;
+	}
+	printf("%d\n", x);
+	return (0);
+}
