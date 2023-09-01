5021a423b8844e43fab36dc5b47ce0bb37a558b7
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-01 14:11:12 -0700

    my commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: Input parameter int
+ * @argv: Input parameter array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
+
