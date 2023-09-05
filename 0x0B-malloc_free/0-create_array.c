f85aafd68aad204660a43616377c216d752c4333
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-05 14:55:01 -0700

    my commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *x = malloc(size);
 
-	if (size == 0 || x == 0)
+	if (size <= 0 || x == 0)
 	{
 		return (NULL);
 	}
-	while (size > 0)
+	while (size >= 0)
 	{
 		x[size] = c;
 		size--;
