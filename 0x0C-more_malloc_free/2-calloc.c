3be05970735c3efe3136ca4b8531bd7e7b36ba27
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-07 15:33:16 -0700

    my commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *fillmemory - fill memory
+ * @str: input parameter string
+ * @b: input parameter string
+ * @n: num of copy
+ * Return: pointer to memory
+ */
+char *fillmemory(char *str, char x, unsigned int y)
+{
+	unsigned int i = 0;
+
+	while (i < y)
+	{
+		str[i] = x;
+		i++;
+	}
+	return (str);
+}
+/**
+ * *_calloc - allocates memory for an array of nmemb elements of size bytes
+ * each and returns a pointer to the allocated memory.
+ * @nmemb: input parameter int
+ * @size: input parameter int
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *strPointer;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	strPointer = malloc(size * nmemb);
+	if (strPointer == NULL)
+		return (NULL);
+	fillmemory(strPointer, 0, nmemb * size);
+	return (strPointer);
+}
