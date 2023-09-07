86cb047ac38d53942c3f6e9f9fdcc1bbe2df978d
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-07 16:29:59 -0700

    my commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,12 +8,15 @@
  */
 int *array_range(int min, int max)
 {
-	int *strPointer = malloc(sizeof(int) * size);
+	int *strPointer;
+
 	int i = 0, size = 0;
 
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
+
+	strPointer = malloc(sizeof(int) * size);
 	if (strPointer == NULL)
 		return (NULL);
 	while (min <= max)
