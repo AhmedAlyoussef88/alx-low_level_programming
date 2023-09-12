d9402cbfd8664ba38cc5c68f6a22b51034709bfc
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-11 17:51:13 -0700

    my commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,9 +6,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return (NULL);
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
