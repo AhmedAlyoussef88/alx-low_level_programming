0d1567dd5639f8ec87816ed780277e910e1851ce
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-13 13:01:16 -0700

    my commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - calculates two integers sum
+ * @a: input parameter int
+ * @b: input parameter int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - two integers differenct
+ * @a: input parameter int
+ * @b: input parameter int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - two integers multiple
+ * @a: input parameter int
+ * @b: input parameter int
+ * Return: multiply
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - two integers division
+ * @a: input parameter int
+ * @b: input parameter int
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - two integers remainder
+ * @a: input parameter int
+ * @b: input parameter int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
