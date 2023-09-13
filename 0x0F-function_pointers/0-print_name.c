467c7bc8bde18ae65a7db8cbd4983b626efaa3ce
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-13 12:23:57 -0700

    my commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - prints a name
+ * @name: input parameter string
+ * @f: input parameter pointer function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		return;
+	}
+	f(name);
+}
