c0a397d6a62221d755316f4774d4eb39de4ffd1e
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-14 17:49:58 -0700

    my commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: input parameter string
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s, *x = "";
+	va_list l;
+
+	va_start(l, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			if (format[i] == 'c')
+				printf("%s%c", x, va_arg(l, int));
+			else if (format[i] == 'i')
+				printf("%s%d", x, va_arg(l, int));
+			else if (format[i] == 'f')
+				printf("%s%f", x, va_arg(l, double));
+			else if (format[i] == 's')
+			{
+				s = va_arg(l, char *);
+				if (!s)
+					s = "(nil)";
+				printf("%s%s", x, s);
+			}
+		x = ", ";
+		i++;
+		}
+	}
+}
