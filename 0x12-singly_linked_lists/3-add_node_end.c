e2c44ff2a543d994a8cfc7c20fac04e1dd2380b7
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-21 09:50:36 -0700

    my commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 /**
  * add_node_end - adds a new node at the end of a list_t list.
  * @head: input parameter list head
