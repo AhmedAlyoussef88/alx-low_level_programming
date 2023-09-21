a12d2170d838268987c0327b5cc90c061f71ac28
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-21 09:27:28 -0700

    my commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: input parameter pointer
+ * Return: str is NULL, print [0] (nil)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t StrSize = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		StrSize++;
+	}
+	return (StrSize);
+}
