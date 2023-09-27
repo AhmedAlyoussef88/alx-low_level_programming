6e481db530736bfd06aab3260406d2fc816b5fa2
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-26 19:11:31 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: input parameter list pointer
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	int x = 0;
+	listint_t *tmpList;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		x = *h - (*h)->next;
+		if (x > 0)
+		{
+			tmpList = (*h)->next;
+			free(*h);
+			*h = tmpList;
+			size++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			size++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (size);
+}
