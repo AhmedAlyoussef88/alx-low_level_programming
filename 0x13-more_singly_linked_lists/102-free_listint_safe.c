63073845f0814771b54b26243c9ce2d007b657d7
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2023-09-26 19:41:57 -0700

    my commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,7 +1,7 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "lists.h"
 /**
  * free_listint_safe - frees a listint_t list.
  * @h: input parameter list pointer
