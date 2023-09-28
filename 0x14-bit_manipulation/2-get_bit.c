f249fbd9a6641e637dc85d6adedb38b0e6bc87de
AhmedAlyoussef88 <138717756+AhmedAlyoussef88@users.noreply.github.com>
2023-09-29 01:17:05 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,13 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int x;
-	
 	if (index >= sizeof(n) * 8)
 	{
 		return (-1);
 	}
-	x = n >> index;
-
-	return (x & 1);
+	return (n >> index & 1);
 }
