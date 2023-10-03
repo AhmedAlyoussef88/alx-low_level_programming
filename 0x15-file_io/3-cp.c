adfcff48884a5f138b4d7a8c568a12381c9751c4
AhmedAlyoussef88 <138717756+AhmedAlyoussef88@users.noreply.github.com>
2023-10-03 12:54:46 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_t\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	b = bufferCreate(argv[2]);
