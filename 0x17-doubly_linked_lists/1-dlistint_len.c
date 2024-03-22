61f070596d56e2bc47bcedaabd7f3d4363c8b175
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-03-21 17:54:45 -0700

    my commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - the number of elements in a linked dlistint_t list.
+ * @h: pointer parameter
+ * Return: number of nodes.
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t cont = 0;
+	
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
+	return cont;
+}
