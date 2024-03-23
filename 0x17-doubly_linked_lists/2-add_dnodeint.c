976eeb5b93973d6c5a63c6fed22dea879154924b
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-03-22 17:12:19 -0700

    my commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,19 +4,20 @@
  * @head: pointer parameter
  * @n: int parameter
  * Return: the address of the new element, or NULL if it failed
- **/
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	new = malloc(sizeof(dlistint_t));
-	
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (*head)
-		(*head)->prev = new;
-	*head = new;
-	return (new);
+        dlistint_t *new;
+        new = malloc(sizeof(dlistint_t));
+
+        if (new == NULL)
+                return (NULL);
+        new->n = n;
+        new->prev = NULL;
+        new->next = *head;
+        if (*head != NULL)
+                (*head)->prev = new;
+        *head = new;
+        return (new);
 }
+
